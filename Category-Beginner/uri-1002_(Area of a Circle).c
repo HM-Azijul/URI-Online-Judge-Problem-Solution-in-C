b228cf58a6ee70aba3a63849790d396f0836eb83
HM Azijul <62802022+HM-Azijul@users.noreply.github.com>
2020-12-08 19:22:09 +0600

    Add files via upload

---
+++ Category-Beginner/uri-1002_(Area of a Circle).c
@@ -0,0 +1,14 @@
+//problem: 1002 (Area of a Circle)
+
+#include <stdio.h>
+int main()
+{
+    const double pi = 3.14159;
+    double R, A;
+
+    scanf("%lf", &R);
+    A = pi * (R * R);
+    printf("A=%.4lf\n", A);
+
+    return 0;
+} */
