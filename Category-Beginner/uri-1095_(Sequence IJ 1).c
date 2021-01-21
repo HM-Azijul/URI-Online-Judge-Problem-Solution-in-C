31f17f7cb68482545bfb650e03b7383245d92f9e
HM Azijul <62802022+HM-Azijul@users.noreply.github.com>
2021-01-22 00:40:25 +0600

    Add files via upload

---
+++ Category-Beginner/uri-1095_(Sequence IJ 1).c
@@ -0,0 +1,17 @@
+
+//* Problem: 1095 (Sequence IJ 1)
+#include <stdio.h>
+int main(void)
+{
+    int i, j = 60;
+
+    for (i = 1; i <= 37; i += 3)
+    {
+        printf("I=%d J=%d\n", i, j);
+        j -= 5;
+    }
+
+    return 0;
+}
+
+
