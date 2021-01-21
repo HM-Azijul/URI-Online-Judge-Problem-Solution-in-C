244e23642a49fd2aabde5dfdff24a48afcc59764
HM Azijul <62802022+HM-Azijul@users.noreply.github.com>
2021-01-22 00:41:40 +0600

    Add files via upload

---
+++ Category-Beginner/uri-1096_(Sequence IJ 2).c
@@ -0,0 +1,18 @@
+
+//* Problem: 1096 (Sequence IJ 2)
+#include <stdio.h>
+int main(void)
+{
+    int i, j;
+
+    for (i = 1; i <= 9; i += 2)
+    {
+        for (j = 7; j >= 5; j--)
+        {
+            printf("I=%d J=%d\n", i, j);
+        }
+    }
+
+    return 0;
+}
+
