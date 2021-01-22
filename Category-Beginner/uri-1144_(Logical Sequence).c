0f45b3184d0bb8746abb4cdb9f4b20716e5b9bdb
HM Azijul <62802022+HM-Azijul@users.noreply.github.com>
2021-01-23 00:10:43 +0600

    Add files via upload

---
+++ Category-Beginner/uri-1144_(Logical Sequence).c
@@ -0,0 +1,17 @@
+
+//* Problem: 1144 (Logical Sequence)
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d %d %d\n", i, i * i, i * i * i);
+        printf("%d %d %d\n", i, i * i + 1, i * i * i + 1);
+    }
+
+    return 0;
+}
+
